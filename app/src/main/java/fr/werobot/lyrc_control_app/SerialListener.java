2f5d738f83b126cecd21656a79c0e3d42157b4e7
Le_Futuriste <spamfree@matthieubessat.fr>
2019-05-04 20:15:31 +0200

    style: serial listener

--- app/src/main/java/fr/werobot/lyrc_control_app/SerialListener.java
+++ app/src/main/java/fr/werobot/lyrc_control_app/SerialListener.java
@@ -1,8 +1,11 @@
 package fr.werobot.lyrc_control_app;
 
 interface SerialListener {
-    void onSerialConnect      ();
-    void onSerialConnectError (Exception e);
-    void onSerialRead         (byte[] data);
-    void onSerialIoError      (Exception e);
+    void onSerialConnect();
+
+    void onSerialConnectError(Exception e);
+
+    void onSerialRead(byte[] data);
+
+    void onSerialIoError(Exception e);
 }
