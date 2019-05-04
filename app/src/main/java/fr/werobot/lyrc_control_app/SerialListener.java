9d6402c84bc5cefe029a7bcb1316d33a6309e6da
Le_Futuriste <spamfree@matthieubessat.fr>
2019-05-04 18:37:45 +0200

    update

---
+++ app/src/main/java/fr/werobot/lyrc_control_app/SerialListener.java
@@ -0,0 +1,8 @@
+package fr.werobot.lyrc_control_app;
+
+interface SerialListener {
+    void onSerialConnect      ();
+    void onSerialConnectError (Exception e);
+    void onSerialRead         (byte[] data);
+    void onSerialIoError      (Exception e);
+}
